252dd1cfe8993bf74aee11840d36fa9ea18af4b1
Your Name <prithuldip@gmail.com>
2022-05-17 16:14:03 +0600

    gg file added in b1

---
+++ app/src/main/java/gg/gg.java
@@ -0,0 +1,28 @@
+package gg;
+
+public class gg {
+    String name;
+    int roll;
+
+    my(String name, int roll)
+    {
+        this.name=name;
+        this.roll=roll;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
